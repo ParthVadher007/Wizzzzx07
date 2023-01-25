f000dcde716e175d6fda0b9a5a2f17a20a920d45
ParthVadher007 <95085046+ParthVadher007@users.noreply.github.com>
2023-01-25 10:59:34 +0530

    Create Par.java

---
+++ Par.java
@@ -0,0 +1,13 @@
+public class Par{
+
+    public static void main(String[] args) {
+
+        String str = "Wizzz";
+
+        int length = str.length();
+
+        System.out.println("Length of the string: " + length);
+
+    }
+
+}
